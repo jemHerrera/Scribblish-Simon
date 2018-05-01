155cd143b0811b76e3b9ee8652cd2389968434ff
jemHerrera <c-laptop@users.github.com>
2018-04-30 23:21:20 -0600

    commit 3

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 
     //variables
-    let animationReady = true;
+    let animationReady = false;
     let hardMode = false;
-    let colorArr = [] 
+    let colorArr = [];
+    let colorInput = '';
+    let inputArr = [];
 
     //game start button
     $('button').on('click', function(){
@@ -18,7 +20,9 @@ $(document).ready(function(){
             else{
                 $('h4').html('Stage 1');
                 clearInterval(countDown);
-            }    
+                getColor();
+                colorRoll(1000);
+            }
             counter--;
         }, 1000);
     });
@@ -27,8 +31,41 @@ $(document).ready(function(){
 
 
 
-    for(var i=1; i<=20; i++){
-        $('h4').html('Stage'+i);
+    //main animation function on keypress
+    $(document).keypress(function(e){
+        if(animationReady == true && [97, 115, 108, 107].indexOf(e.keyCode) != -1){
+            if(e.keyCode == 97) colorInput = 'red';
+            else if(e.keyCode == 115) colorInput = 'yellow';
+            else if(e.keyCode == 108) colorInput = 'blue';
+            else colorInput = 'green';
+
+            inputArr.push(colorInput);
+            if (colorArr[inputArr.length-1] != colorInput) {
+                flipAnimation(colorInput, 500 ,false);
+                animationReady = false;
+                inputArr = [];
+                setTimeout(() => colorRoll(1000),1000);
+            }
+            else{
+                flipAnimation(colorInput, 500 ,true);
+                if (colorArr.length == inputArr.length){
+                    inputArr = [];
+                    getColor();
+                    colorRoll(1000);
+                }
+            }
+        }
+    });
+
+
+
+
+
+
+
+
+    //FUNCTIONS
+    function getColor(){
         let randomKey = Math.floor((Math.random()*4)+1);
         let color = '';
         switch(randomKey){
@@ -46,31 +83,40 @@ $(document).ready(function(){
                 break;
         }
         colorArr.push(color);
-        colorArr.forEach(color => {
-            flipAnimation(color, 500);
-        });
-    }   
+    }
 
-
-    
-
-    //main animation function on keypress
-    $(document).keypress(function(e){
-        if(animationReady == true && [97, 115, 108, 107].indexOf(e.keyCode) != -1){
-            if(e.keyCode == 97) flipAnimation('red', 500);
-            else if(e.keyCode == 115)flipAnimation('yellow', 500);
-            else if(e.keyCode == 108)flipAnimation('blue', 500);
-            else flipAnimation('green', 500);
-        }
-        function flipAnimation(id, speed){
+    function colorRoll(ms){
+        let counter = 0;
+        let roll = setInterval(function(){
+            flipAnimation(colorArr[counter], 500, true);
+            counter++;
             animationReady = false;
-            $('.'+id).css('animation', 'jump  '+speed+'ms cubic-bezier(0.48, 0.16, 0.5, 0.75) alternate 2');
-            $('.'+id).css('transition', 'color '+speed*2+'ms cubic-bezier(0.48, 0.16, 0.5, 0.75)');
-            $('.'+id).css('background-color', 'var(--'+id+')');
-            setTimeout(function(){
-                $('.'+id).css('animation', '').css('background-color', '');
+            if(counter == colorArr.length) {
+                clearInterval(roll);
                 animationReady = true;
-            }, speed*1.6);
-        }
-    });
+                console.log(colorArr);
+            }
+        }, ms);
+    }
+
+
+
+    function colorCheck(input){
+
+    }
+
+
+
+    function flipAnimation(id, speed, boolean){
+        animationReady = false;
+        $('.'+id).css('animation', 'jump  '+speed+'ms cubic-bezier(0.48, 0.16, 0.5, 0.75) alternate 2');
+        $('.'+id).css('transition', 'color '+speed*2+'ms cubic-bezier(0.48, 0.16, 0.5, 0.75)');
+        if (boolean == true)$('.'+id).css('background-color', 'var(--'+id+')');
+        else $('.'+id).css('background-color', 'none');
+        setTimeout(function(){
+            $('.'+id).css('animation', '').css('background-color', '');
+            animationReady = true;
+        }, speed*1.6);
+    }
 });
+
